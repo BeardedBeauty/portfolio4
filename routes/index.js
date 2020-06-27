dfaa5fcd2b79a62e2f447252319dab2f4ed4c5ea
John Ochs <john.w.ochs0@gmail.com>
2020-06-26 22:02:02 -0500

    Server runs blank slate

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+
+const router = require("express").Router();
+// const route = require("./api/index.js");
+
+// user routes
+// router.use("/api", route);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const path = require("path");
 const routes = require("./routes");
 require("dotenv").config();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "front-end/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 const PORT = process.env.PORT || 3009;
 let mdb = process.env.MONGODB_URI;
 
-mongoose.connect(mdb, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log(`Database connected successfully`);
-}).catch(err => console.log(err));
+// mongoose.connect(mdb, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//     console.log(`Database connected successfully`);
+// }).catch(err => console.log(err));
 
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "front-end", "build", "index.html")));
 
